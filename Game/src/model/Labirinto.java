9fa82ad78c1d93458cf61f1c69541ff67a318793
Diovane Mendes <diovane.m.mendes@gmail.com>
2017-05-28 03:43:13 -0300

    Leitura de arquivo texto
    Leitura de arquivo texto

---
+++ Game/src/model/Labirinto.java
@@ -0,0 +1,13 @@
+package model;
+
+import com.senac.SimpleJava.Graphics.Color;
+import com.senac.SimpleJava.Graphics.Sprite;
+
+public class Labirinto extends Sprite {
+
+	private int delta = 0;
+
+	public Labirinto() {
+		super(25, 5, Color.RED);
+	}
+}
